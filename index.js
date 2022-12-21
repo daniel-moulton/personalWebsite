4cfb7f79beb45438ad22ab6322e230223a41db42
Daniel Moulton <dan.moulton@btinternet.com>
2022-12-21 01:45:49 +0000

    captcha

--- index.js
+++ index.js
@@ -25,8 +25,14 @@ window.addEventListener('scroll', () => {
 });
 
 window.onbeforeunload = () => {
-    for(const form of document.getElementsByTagName('form')) {
-      form.reset();
+    for (const form of document.getElementsByTagName('form')) {
+        form.reset();
     }
+}
+
+window.onload = function() { 
+    var el = document.getElementById('g-recaptcha-response'); 
+    if (el) { 
+      el.setAttribute('required', 'required'); 
+    } 
   }
-  
