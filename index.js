2e0abc42afa26c01069924b723893a6aa4b6019f
Daniel Moulton <dan.moulton@btinternet.com>
2022-12-22 11:32:40 +0000

    favicon + recaptcha

--- index.js
+++ index.js
@@ -30,9 +30,38 @@ window.onbeforeunload = () => {
     }
 }
 
-window.onload = function() { 
-    var el = document.getElementById('g-recaptcha-response'); 
-    if (el) { 
-      el.setAttribute('required', 'required'); 
-    } 
-  }
+window.onload = function () {
+    var el = document.getElementById('g-recaptcha-response');
+    if (el) {
+        el.setAttribute('required', 'required');
+    }
+}
+
+let slideIndex = 1;
+showSlides(slideIndex);
+
+// Next/previous controls
+function plusSlides(n) {
+    showSlides(slideIndex += n);
+}
+
+// Thumbnail image controls
+function currentSlide(n) {
+    showSlides(slideIndex = n);
+}
+
+function showSlides(n) {
+    let i;
+    let slides = document.getElementsByClassName("gallery");
+    let dots = document.getElementsByClassName("dot");
+    if (n > slides.length) { slideIndex = 1 }
+    if (n < 1) { slideIndex = slides.length }
+    for (i = 0; i < slides.length; i++) {
+        slides[i].style.display = "none";
+    }
+    for (i = 0; i < dots.length; i++) {
+        dots[i].className = dots[i].className.replace(" active", "");
+    }
+    slides[slideIndex - 1].style.display = "block";
+    dots[slideIndex - 1].className += " active";
+}
